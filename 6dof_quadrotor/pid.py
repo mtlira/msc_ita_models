805913e03d3883e14296d8858246402f15f1c3d3
mtlira <matheusribeiro10@gmail.com>
2024-10-05 11:22:10 -0300

    code refactored. plots, pid and linearization codes separated in individual files

--- 6dof_quadrotor/pid.py
+++ 6dof_quadrotor/pid.py
@@ -1,3 +1,6 @@
+import numpy as np
+from scipy.integrate import odeint
+
 class PID(object):
     def __init__(self, KP, KI, KD, setpoint, time_step):
         self.KP = KP
@@ -20,4 +23,26 @@ class PID(object):
         self.previous_error = self.error
         self.output = self.proportional_error + self.integral_error + self.derivative_error
         return self.output
+    
+    def simulate(self, t, X0, u_, time_step, f):
+        n_steps = len(t)
+        phi_feedback = X0[0]
+        X_old = X0
+        X_vector = [X0]
+        tx_vector = []
+        u_pid = u_(0)
+        f_t, t_x, t_y, t_z = u_pid
+
+        for i in range(1, n_steps):
+            t_i = i*time_step
+            t_i_old = (i-1)*time_step
+            t_vector = np.linspace(t_i_old, t_i, 10)
+            tx_pid = self.compute(phi_feedback)
+            #t_x = tx_pid
+            X = odeint(f, X_old,t_vector, args=(f_t, tx_pid, t_y, t_z)) # Only tx is being controlled
+            phi_feedback = X[-1][0]
+            X_old = X[-1]
+            X_vector.append(X[-1])
+            tx_vector.append(tx_pid)
+        return X_vector
 
