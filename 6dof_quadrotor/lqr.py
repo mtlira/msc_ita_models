fd62011fbbd54044f72c612a9f07ee17d95fae0a
mtlira <matheusribeiro10@gmail.com>
2025-01-21 20:47:18 -0300

    lqr code refactored

--- 6dof_quadrotor/lqr.py
+++ 6dof_quadrotor/lqr.py
@@ -1,30 +1,77 @@
 import numpy as np
 from scipy.integrate import odeint
+from control import lqr
+from scipy.signal import StateSpace, lsim
 
 class LQR(object):
-    def __init__(self, K, K_i, C, time_step, time_sample):
-        self.K = K
-        self.K_i = K_i # Integral contgrol gain
+    def __init__(self, A, B, C, time_step, time_sample):
+        self.A = A
+        self.B = B
+        self.C = C
+        #self.K = K
+        #self.K_i = K_i # Integral contgrol gain
         self.integral_error = 0 # Sum of (r-y)*dt
         self.output_error = 0 # r - y
-        self.C = C
         self.time_step = time_step # Simulation time
         self.time_sample = time_sample # Controller time sample
+
+    def initialize(self, x_max, u_max):
+        """
+        Builds weight matrices Q and R using Bryson's rule the LQR gain K and the augmented matrices and gain (K_aug) needed for integral action.
+        """
+        Q = np.eye(12)
+        for i in range(len(Q)):
+            Q[i][i] = 1/(x_max[i]**2)
+        self.Q = Q
+
+        R = np.eye(4)
+        for i in range(len(R)):
+            R[i][i] = 1/(u_max[i]**2)
+        self.R = R
+
+        A_a = np.concatenate((self.A, np.zeros((12,3))), axis = 1)
+        A_a2 = np.concatenate((-self.C, np.zeros((3,3))), axis = 1)
+        A_a = np.concatenate((A_a, A_a2), axis = 0)
+        self.A_a = A_a
+
+        B_a = np.concatenate((self.B, np.zeros((3,4))),axis = 0)
+        self.B_a = B_a
+
+        G = np.concatenate((np.zeros((12,3)), np.eye(3)), axis = 0)
+        self.G = G
+
+        # LQR augmented
+        x_max_aug = np.concatenate((x_max, np.array([1,1,1])), axis = 0)
+        Q_aug = np.eye(15)
+        for i in range(len(Q_aug)):
+            Q_aug[i][i] = 1/(x_max_aug[i]**2)
+
+        K_a, _, _ = lqr(A_a, B_a, Q_aug, R) # TODO: Talvez tenha que aumentar Q e R
+        self.K_a = K_a
+        self.K = K_a[:,0:12] # LQR gain K from augmented LQR version
+        self.K_i = K_a[:,12:] # Integrator gain
+
+        self.C_a = np.array([ # TODO: AUTOMATIZAR CRIAÇÃO
+                        [0,0,0,0,0,0,0,0,0,1,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0]])  
+
 
     def compute(self, reference, x_feedback):
         output = np.matmul(self.C,x_feedback)
         reference_error = reference - output
-        self.integral_error += reference_error*self.time_step
+        self.integral_error += reference_error*self.time_sample
         u = -np.matmul(self.K,x_feedback) - np.matmul(self.K_i,self.integral_error)
         return u
     
     def simulate(self, X0, t, ref_vector, f, u_eq):
+        self.integral_error = 0
+        self.output_error = 0
         n_steps = len(t)
         x_feedback = X0
         X_vector = [X0]
         u_vector = []
         reference = ref_vector[0]
-        print(self.time_step,2*self.time_step,3*self.time_step)
 
         for i in range(1, n_steps):
             u_i = u_eq + self.compute(reference, x_feedback)
@@ -42,17 +89,18 @@ class LQR(object):
         return np.array(X_vector), np.array(u_vector)
 
     def simulate2(self, X0, t_samples, ref_vector, f, u_eq):
+        self.integral_error = 0
+        self.output_error = 0
         x_k = X0
         X_vector = [X0]
         u_vector = []
-        print(t_samples[1],t_samples[2],t_samples[3])
 
         for k in range(0, len(t_samples) - 1):
             reference = ref_vector[k]
             u_k = u_eq + self.compute(reference, x_k)
             f_t_i, t_x_i, t_y_i, t_z_i = u_k
-            t_vector = np.arange(t_samples[k], t_samples[k+1], self.time_step) if self.time_sample < self.time_step else [t_samples[k], t_samples[k+1]]
-            #t_vector = np.linspace(t_samples[k], t_samples[k+1], 10)
+            t_vector = np.arange(t_samples[k], t_samples[k+1], self.time_step) if self.time_step < self.time_sample else [t_samples[k], t_samples[k+1]]
+            #t_vector = np.linspace(t_samples[k], t_samples[k+1], 100)
             x_k = odeint(f, x_k, t_vector, args = (f_t_i, t_x_i, t_y_i, t_z_i)) # TODO: Verificar se t_vector está correto (provavelmente não)
             #x_feedback = odeint(f, x_old, [t_i_old, t_i], args = (f_t_i, t_x_i, t_y_i, t_z_i))
             x_k = x_k[-1]
@@ -60,3 +108,32 @@ class LQR(object):
             u_vector.append(u_k)
 
         return np.array(X_vector), np.array(u_vector)
+
+    def simulate_linear(self, X0, t, r_tracking):
+        self.integral_error = 0
+        self.output_error = 0
+        linear_sys_tracking = StateSpace(self.A_a - np.matmul(self.B_a, self.K_a), self.G, self.C_a, np.zeros((3,3)))
+        X0_aug = np.concatenate((X0, [0,0,0]), axis=0)
+
+        _,_, x_tracking = lsim(linear_sys_tracking, r_tracking, t, X0 = X0_aug)
+        return x_tracking
+    
+
+#DRAFT
+# LQR - state regulation only (no tracking)
+#linear_sys = signal.StateSpace(A - B*K,B,np.eye(np.shape(A)[0]), np.zeros((np.shape(A)[0],np.shape(B)[1])))
+#_, _, delta_xout = signal.lsim(linear_sys, 0, t, X0 = X0 - X_eq)
+#xout = X_eq + delta_xout
+
+#eig_Acl, _ = np.linalg.eig(np.array(A-B*K, dtype='float'))
+#print('Eigenvalues of A - B*K (closed-loop):')
+#print(eig_Acl)
+#plot_states(xout, t)
+
+#linear_sys2 = ct.StateSpace(A - B*K, B, np.eye(np.shape(A)[0]), np.zeros((np.shape(A)[0],np.shape(B)[1])))
+#ct_out = ct.forced_response(linear_sys2, t, 0, X0 - X_eq, transpose=True)
+#xout2 = X_eq + ct_out.states
+
+#eig_Acl_a, _ = np.linalg.eig(np.array(A_a - np.matmul(B_a,K_a), dtype='float'))
+#print('Eigenvalues of Aa - Ba*Ka (closed-loop):')
+#print(eig_Acl_a)
--- 6dof_quadrotor/quadrotor_6dof.py
+++ 6dof_quadrotor/quadrotor_6dof.py
@@ -24,9 +24,9 @@ KD = 3
 KI = 3
 phi_setpoint = 0
 
-time_step = 1e-2 #5e-3 é um bom valor
-T_sample = 5e-2 # MP sample time
-T_simulation = 20
+time_step = 1e-4 #5e-3 é um bom valor
+T_sample = 1e-2 # MP sample time
+T_simulation = 15
 
 t = np.arange(0,T_simulation, time_step)
 t_samples = np.arange(0,T_simulation, T_sample)
@@ -168,6 +168,10 @@ X = odeint(f, y0=X0, t=t)
 #print('eq point:',root)
 
 A,B = linearize(f_sym, X_eq, u_eq)
+C = np.array([[0,0,0,0,0,0,0,0,0,1,0,0],
+              [0,0,0,0,0,0,0,0,0,0,1,0],
+              [0,0,0,0,0,0,0,0,0,0,0,1]])
+
 _, _, x_lin = openloop_sim_linear(A, B, t, X0, X_eq, u_eq, u_sim)
 
 eig_A, _ = np.linalg.eig(np.array(A, dtype='float'))
@@ -196,67 +200,8 @@ u_max = [
     1.8*m*g+0*I_z*(x_max[2])/1.5
     ]
 
-Q = np.eye(12)
-for i in range(len(Q)):
-    Q[i][i] = 1/(x_max[i]**2)
-
-R = np.eye(4)
-for i in range(len(R)):
-    R[i][i] = 1/(u_max[i]**2)
-
-#Q = np.eye(12)
-#R = np.eye(4)
-
-K, _, _ = ct.lqr(A, B, Q, R)
-
-# LQR - state regulation only (no tracking)
-linear_sys = signal.StateSpace(A - B*K,B,np.eye(np.shape(A)[0]), np.zeros((np.shape(A)[0],np.shape(B)[1])))
-_, _, delta_xout = signal.lsim(linear_sys, 0, t, X0 = X0 - X_eq)
-xout = X_eq + delta_xout
-
-eig_Acl, _ = np.linalg.eig(np.array(A-B*K, dtype='float'))
-print('Eigenvalues of A - B*K (closed-loop):')
-print(eig_Acl)
-#plot_states(xout, t)
-
-#linear_sys2 = ct.StateSpace(A - B*K, B, np.eye(np.shape(A)[0]), np.zeros((np.shape(A)[0],np.shape(B)[1])))
-#ct_out = ct.forced_response(linear_sys2, t, 0, X0 - X_eq, transpose=True)
-#xout2 = X_eq + ct_out.states
-
-#print('shape states lsim =',np.shape(xout))
-#print('shape states control =',np.shape(xout2))
-
-#plot_states(xout,t,xout2)
-
 ########################################################################################
 # LQR - tracking
-# Attempt 1 - Integrator 
-C = np.array([[0,0,0,0,0,0,0,0,0,1,0,0],
-              [0,0,0,0,0,0,0,0,0,0,1,0],
-              [0,0,0,0,0,0,0,0,0,0,0,1]])
-
-A_a = np.concatenate((A, np.zeros((12,3))), axis = 1)
-A_a2 = np.concatenate((-C, np.zeros((3,3))), axis = 1)
-A_a = np.concatenate((A_a, A_a2), axis = 0)
-A_a2 = _
-
-B_a = np.concatenate((B, np.zeros((3,4))),axis = 0)
-
-G = np.concatenate((np.zeros((12,3)), np.eye(3)), axis = 0)
-
-# LQR augmented
-x_max_aug = np.concatenate((x_max, np.array([1,1,1])), axis = 0)
-Q_aug = np.eye(15)
-for i in range(len(Q_aug)):
-    Q_aug[i][i] = 1/(x_max_aug[i]**2)
-K_a, _, _ = ct.lqr(A_a, B_a, Q_aug, R) # TODO: Talvez tenha que aumentar Q e R
-K2 = K_a[:,0:12] # LQR gain K from augmented LQR version
-K_i = K_a[:,12:] # Integrator gain
-
-C_a = np.array([
-                [0,0,0,0,0,0,0,0,0,1,0,0,0,0,0],
-                [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0],
-                [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0]])
 
 w = 2*np.pi*1/20
 r_helicoidal = np.array([5*(1 + 0.1*t)*np.sin(w*t),
@@ -299,25 +244,21 @@ r_explode = np.array([np.zeros(len(t)),
                        np.zeros(len(t)),
                        (-20*np.ones(len(t)))]).transpose()
 
-r_tracking = r_circle_xy
+r_tracking = r_circle_xy2
 
-linear_sys_tracking = signal.StateSpace(A_a - np.matmul(B_a,K_a), G, C_a, np.zeros((3,3)))
-X0_aug = np.concatenate((X0, [0,0,0]), axis=0)
-X_eq= np.concatenate((X_eq, [0,0,0]), axis=0)
+LQR = lqr.LQR(A, B, C, time_step, T_sample)
+LQR.initialize(x_max, u_max)
 
-eig_Acl_a, _ = np.linalg.eig(np.array(A_a - np.matmul(B_a,K_a), dtype='float'))
-print('Eigenvalues of Aa - Ba*Ka (closed-loop):')
-print(eig_Acl_a)
-
-_,_, x_tracking = signal.lsim(linear_sys_tracking,r_tracking,t, X0 = X0_aug)
-x_lqr_linear = x_tracking
-
-#lqr1 = lqr.LQR(K2, K_i, C, time_step, T_sample)
-#lqr2 = lqr.LQR(K2, K_i, C, time_step, T_sample)
+# Nonlinear simulation
+x_lqr_nonlinear, u_lqr_nonlinear = LQR.simulate2(X0, t_samples, r_tracking, f2, u_eq)
+#print('shape nonlinear', np.shape(x_lqr_nonlinear))
+# Linear simulation
+x_lqr_linear = LQR.simulate_linear(X0, t_samples, r_tracking)
+#print('shape linear', np.shape(x_lqr_linear))
 
 #X_lqr_nonlinear, u_vector = lqr1.simulate(X0, t, r_tracking, f2, u_eq) # Não linear
 #X_lqr_nonlinear2, u_vector2 = lqr2.simulate2(X0, t_samples, r_tracking, f2, u_eq)
-#plot_states(X_lqr_nonlinear, t, x_lqr_linear, r_tracking)
+plot_states(x_lqr_nonlinear, t_samples, x_lqr_linear, r_tracking)
 #r_tracking = r_circle_xy2
 #plot_states(X_lqr_nonlinear2, t_samples, X_lin = None, trajectory=r_tracking)
 #plot_inputs(u_vector,t[0:-1])
@@ -417,6 +358,6 @@ control_weights = 1 / (M*restrictions['delta_u_max']**2)
 MPC = mpc.mpc(M, N, rho, A, B, C, time_step, T_sample, output_weights, control_weights, restrictions)
 MPC.initialize_matrices()
 X_mpc_nonlinear, u_mpc = MPC.simulate(f2, X0, t_samples, r_tracking, u_eq)
-#X_mpc_linear, u_mpc_linear = MPC.simulate_linear(X0, t_samples, r_tracking, u_eq)
-plot_states(X_mpc_nonlinear, t_samples[:np.shape(X_mpc_nonlinear)[0]], X_mpc_nonlinear, r_tracking, u_mpc)
+X_mpc_linear, u_mpc_linear = MPC.simulate_linear(X0, t_samples, r_tracking, u_eq)
+plot_states(X_mpc_nonlinear, t_samples[:np.shape(X_mpc_nonlinear)[0]], X_mpc_linear, r_tracking, u_mpc)
 #plot_inputs(u_mpc, t_samples[0:-1])
