switch between normal and gain scheduled mpc, RMSe formula corrected (need to be deployed)

6dof_quadrotor/mpc.py
@@ -914,6 +914,12 @@ class MPC(object):
         #     sys_d = cont2discrete((self.A,self.B,self.C, np.zeros((self.q,self.p))), self.T_sample, 'zoh')
         #     Ad, Bd, Cd, _, _ = sys_d
         #     return Ad, Bd, Cd
+    
+    def RMSe(self, position, trajectory):
+        delta_position = trajectory - position   # shape (N, 3)
+        squared_norms = np.sum(delta_position**2, axis=1)  # Sum over x, y, z for each time step
+        RMSe = np.sqrt(np.mean(squared_norms))
+        return RMSe
 
 class GainSchedulingMPC(object):
     def __init__(self, model, phi_grid_deg, theta_grid_deg, M, N, time_step, T_sample, output_weights, control_weights,\
@@ -1052,7 +1058,7 @@ class GainSchedulingMPC(object):
             if disturb_input and k>0:
                 probability = np.random.rand()
                 if probability > disturb_frequency:
-                    uu_k = self.add_input_disturbance(uu_k, model)                
+                    uu_k = linear_model.add_input_disturbance(uu_k, model)                
 
             # Apply control u_k in the multi-rotor
             f_t_k, t_x_k, t_y_k, t_z_k = uu_k # Attention for u_eq (solved the problem)
@@ -1097,7 +1103,7 @@ class GainSchedulingMPC(object):
                 ref_N_relative = ref_N_position - position_k
 
                 # Clarification: u is actually (u - ueq) and delta_u is (u-ueq)[k] - (u-ueq)[k-1] in this MPC formulation (i.e., u is in reference to u_eq, not 0)
-                NN_sample = np.concatenate((NN_sample, x_k_old[0:9], ref_N_relative, self.restrictions['u_max'] + linear_model.u_ref, u_k), axis = 0) #TODO: depois, acrescentar restrições
+                NN_sample = np.concatenate((NN_sample, x_k_old[0:9], ref_N_relative, linear_model.restrictions['u_max'] + linear_model.u_ref, u_k), axis = 0) #TODO: depois, acrescentar restrições
 
                 NN_dataset.append(NN_sample)
                 end_waste_time = time.time()
@@ -1112,6 +1118,9 @@ class GainSchedulingMPC(object):
         position = X_vector[:, 9:]
         delta_position = trajectory[:len(position),:3] - position
         RMSe = np.sqrt(np.mean(delta_position**2))
+        #RMSe = linear_model.RMSe(position, trajectory[:len(position),:3])
+
+        #print('RMSe',RMSe, '\nWrong RMSe',RMSe_wrong)
 
         min_phi = np.min(X_vector[:,0])
         max_phi = np.max(X_vector[:,0])

6dof_quadrotor/neural_network.py
@@ -54,7 +54,7 @@ class ControlAllocationDataset_Binary(Dataset):
         self.dataset = np.concatenate(self.dataset, axis = 0)
         self.num_inputs = len(self.dataset[0]) - num_outputs
         
-        print(f'\tLoaded {len(self.dataset)} datasets')
+        print(f'\tLoaded {len(self.dataset)} samples')
         print(f'\tSample length: {len(self.dataset[0])}')
         print(f'\tDataset size: {self.dataset.nbytes / 1024**2} MB')
 
@@ -244,7 +244,7 @@ if __name__ == '__main__':
     pass
     #Teste
     #teste = ControlAllocationDataset_Split('teste/', False, num_rotors)
-    teste = ControlAllocationDataset_Binary('teste/', False, num_rotors)
+    teste = ControlAllocationDataset_Binary('simulations/04_27_00h-26m/', False, num_rotors)
     print('first sample\n',teste.__getitem__(0))
     print(teste.num_inputs,teste.num_outputs)
 

6dof_quadrotor/simulate_mpc.py
@@ -28,7 +28,7 @@ from parameters.octorotor_parameters import m, g, I_x, I_y, I_z, l, b, d, num_ro
 model = multirotor.Multirotor(m, g, I_x, I_y, I_z, b, l, d, num_rotors, thrust_to_weight)
 
 ### SIMULATION PARAMETERS ###
-from parameters.simulation_parameters import time_step, T_sample, N, M
+from parameters.simulation_parameters import time_step, T_sample, N, M, include_phi_theta_reference, include_psi_reference, gain_scheduling
 
 
 # Input and state values at the equilibrium condition
@@ -82,7 +82,8 @@ dataset_dataframe['disturbed_inputs'] = dataset_dataframe['disturbed_inputs'].as
 
 # MPC Implementation
 
-def simulate_mpc(X0, time_step, T_sample, T_simulation, trajectory, restrictions, output_weights, control_weights, dataset_name=None, folder_name=None, disturb_input=False):
+def simulate_mpc(X0, time_step, T_sample, T_simulation, trajectory, restrictions, output_weights, control_weights,\
+                  gain_scheduling, dataset_name=None, folder_name=None, disturb_input=False):
     '''
     Executes a control simulation with MPC given the time step, time sample, total simulation time and the desired trajectory.\n
     dataset_name: name of dataset folder the current simulation will belong to\n
@@ -95,10 +96,22 @@ def simulate_mpc(X0, time_step, T_sample, T_simulation, trajectory, restrictions
     t_samples = np.arange(0,T_simulation, T_sample)
 
     Bw = B @ model.Gama
-    MPC = mpc.MPC(M, N, A, Bw, C, time_step, T_sample, output_weights, control_weights, restrictions, omega_eq**2)
-    MPC.initialize_matrices()
     try:
-        x_mpc_rotors, u_rotors, omega_vector, NN_dataset, metadata = MPC.simulate_future_rotors(model, X0, t_samples, trajectory, generate_dataset=generate_dataset, disturb_input=disturb_input)
+        if not gain_scheduling:
+            MPC = mpc.MPC(M, N, A, Bw, C, time_step, T_sample, output_weights, control_weights, restrictions, omega_eq**2)
+            MPC.initialize_matrices()
+            x_mpc_rotors, u_rotors, omega_vector, NN_dataset, metadata = \
+                    MPC.simulate_future_rotors(model, X0, t_samples,trajectory, generate_dataset=generate_dataset,\
+                                                disturb_input=disturb_input)
+        else:
+            phi_grid = np.array([-75, -60, -45, -30, -15, 0, 15, 30, 45, 60, 75])
+            theta_grid = np.array([-75, -60, -45, -30, -15, 0, 15, 30, 45, 60, 75])
+            gain_MPC = mpc.GainSchedulingMPC(model, phi_grid, theta_grid, M, N, time_step, T_sample, output_weights, \
+                control_weights, restrictions, include_psi_reference, include_phi_theta_reference)
+            x_mpc_rotors, u_rotors, omega_vector, NN_dataset, metadata = \
+                gain_MPC.simulate_future_rotors(model, X0, t_samples, trajectory, generate_dataset=generate_dataset, \
+                                                disturb_input=disturb_input)
+                
     except Exception as error:
         x_mpc_rotors, u_rotors, omega_vector, NN_dataset, metadata = None, None, None, None, None
         print('exception:', error)
@@ -110,7 +123,8 @@ def simulate_mpc(X0, time_step, T_sample, T_simulation, trajectory, restrictions
         #current_time = now.strftime("%m_%d_%Hh-%Mm")
         if dataset_name is not None:
             Path(save_path).mkdir(parents=True, exist_ok=True)
-            np.savetxt(save_path + "dataset.csv", NN_dataset, delimiter=",")
+            #np.savetxt(save_path + "dataset.csv", NN_dataset, delimiter=",")
+            np.save(save_path + 'dataset.npy', NN_dataset.astype(np.float32))
         plot_states(x_mpc_rotors, t_samples[:np.shape(x_mpc_rotors)[0]], trajectory=trajectory, u_vector=u_rotors, omega_vector=omega_vector, equal_scales=True, legend=['MPC', 'Trajectory'], save_path=save_path)
         return True, metadata, simulation_data
     return False, None, None
@@ -132,17 +146,18 @@ def simulate_batch(trajectory_type, args_vector, restrictions_vector, simulate_d
                 folder_name = f'{trajectory_type}/' + str(dataset_id)
 
                 # Simulation without disturbances
-                print(f'Simulation {dataset_id}/{total_simulations}')
+                print(f'{trajectory_type} Simulation {dataset_id}/{total_simulations}')
                 T_simulation = args[-1]
-                simulation_success, simulation_metadata, _ = simulate_mpc(X0, time_step, T_sample, T_simulation, trajectory, restrictions, output_weights, control_weights, dataset_name, folder_name, disturb_input = False)
+                simulation_success, simulation_metadata, _ = simulate_mpc(X0, time_step, T_sample, T_simulation, trajectory, \
+                    restrictions, output_weights, control_weights, gain_scheduling, dataset_name, folder_name, disturb_input = False)
 
-                if not simulation_success:
+                #if not simulation_success:
                     #restrictions_hover = restrictions.copy()
                     #restrictions_hover['y_max'][3:5] /= 2
                     #restrictions_hover['y_min'] = -restrictions_hover['y_max'][3:5]
-                    output_weights_hover = np.copy(output_weights)
-                    output_weights_hover[3:5] *= 4 # Divide delta_y_max by 2 for phi and theta
-                    simulation_success, simulation_metadata, _ = simulate_mpc(X0, time_step, T_sample, T_simulation, trajectory, restrictions, output_weights_hover, control_weights, dataset_name, folder_name, disturb_input = False)
+                #    output_weights_hover = np.copy(output_weights)
+                #    output_weights_hover[3:5] *= 4 # Divide delta_y_max by 2 for phi and theta
+                #    simulation_success, simulation_metadata, _ = simulate_mpc(X0, time_step, T_sample, T_simulation, trajectory, restrictions, output_weights_hover, control_weights, dataset_name, folder_name, disturb_input = False, gain_scheduling=True)
 
                 simulation_metadata = {
                     'sim_id': dataset_id,
@@ -180,16 +195,18 @@ def simulate_batch(trajectory_type, args_vector, restrictions_vector, simulate_d
 
                 # Simulation with disturbances
                 if simulate_disturbances:
-                    print(f'Simulation {dataset_id}/{total_simulations}')
-                    simulation_success, simulation_metadata, _ = simulate_mpc(X0, time_step, T_sample, T_simulation, trajectory, restrictions, output_weights, control_weights, dataset_name, folder_name, disturb_input = True)
+                    print(f'{trajectory_type} Simulation {dataset_id}/{total_simulations}')
+                    simulation_success, simulation_metadata, _ = \
+                        simulate_mpc(X0, time_step, T_sample, T_simulation, trajectory, restrictions, output_weights, \
+                                     control_weights, gain_scheduling, dataset_name, folder_name, disturb_input = True)
                     
-                    if not simulation_success:
-                        #restrictions_hover = restrictions.copy()
-                        #restrictions_hover['y_max'][3:5] /= 2
-                        #restrictions_hover['y_min'] = -restrictions_hover['y_max'][3:5]
-                        output_weights_hover = np.copy(output_weights)
-                        output_weights_hover[3:5] *= 4 # Divide delta_y_max by 2 for phi and theta
-                        simulation_success, simulation_metadata = simulate_mpc(X0, time_step, T_sample, T_simulation, trajectory, restrictions, output_weights_hover, control_weights, dataset_name, folder_name, disturb_input = True)
+                    #if not simulation_success:
+                    #    #restrictions_hover = restrictions.copy()
+                    #    #restrictions_hover['y_max'][3:5] /= 2
+                    #    #restrictions_hover['y_min'] = -restrictions_hover['y_max'][3:5]
+                    #    output_weights_hover = np.copy(output_weights)
+                    #    output_weights_hover[3:5] *= 4 # Divide delta_y_max by 2 for phi and theta
+                    #    simulation_success, simulation_metadata = simulate_mpc(X0, time_step, T_sample, T_simulation, trajectory, restrictions, output_weights_hover, control_weights, dataset_name, folder_name, disturb_input = True)
 
 
                     simulation_metadata = {
@@ -224,7 +241,7 @@ def simulate_batch(trajectory_type, args_vector, restrictions_vector, simulate_d
                     if not simulation_success: failed_simulations += 1
                     dataset_id += 1
                     dataset_dataframe = pd.concat([dataset_dataframe, simulation_metadata])
-                if int(dataset_id) % 50 <= 1:
+                if int(dataset_id) % 20 <= 1:
                     dataset_dataframe.to_csv(dataset_save_path, sep=',', index = False)
             else:
                 dataset_id += 1
@@ -255,29 +272,29 @@ def generate_dataset(dataset_name = None):
     # 2. Generation of trajectory batches and simulation of batches
     if generate_point:
         T_simulation_point = 10
-        num_points = 15
+        num_points = 50
         points_args = tr.generate_point_trajectories(num_points, T_simulation_point)
-        simulate_batch('point', points_args, restrictions_performance, simulate_disturbances = True)
+        simulate_batch('point', points_args, restrictions_performance, simulate_disturbances = True,dataset_save_path=dataset_save_path)
     
     if generate_circle_xy:
         circle_xy_args = tr.generate_circle_xy_trajectories()
         simulate_batch('circle_xy', circle_xy_args, restrictions_performance, simulate_disturbances = True, dataset_save_path=dataset_save_path)
 
     if generate_line:
-        num_lines = 50
+        num_lines = 35
         line_args = tr.generate_line_trajectories(num_lines)
         simulate_batch('line', line_args, restrictions_performance, simulate_disturbances = True, dataset_save_path=dataset_save_path)
 
     if generate_circle_xz:
         circle_xz_args = tr.generate_circle_xz_trajectories()
-        simulate_batch('circle_xz', circle_xz_args, restrictions_performance, simulate_disturbances = True, dataset_save_path=dataset_save_path, checkpoint_id = 301)
+        simulate_batch('circle_xz', circle_xz_args, restrictions_performance, simulate_disturbances = True, dataset_save_path=dataset_save_path)
 
     if generate_lissajous_xy:
         lissajous_xy_args = tr.generate_lissajous_xy_trajectories()
         simulate_batch('lissajous_xy', lissajous_xy_args, restrictions_performance, simulate_disturbances = False, dataset_save_path=dataset_save_path)
 
     if simulate_fault_tolerance:
-        fault_tolerance_args = [[0, 0, 0, 15], [0,0,1,15], [0,0,-1,15]]
+        fault_tolerance_args = [[0, 0, 0, 15], [0,0,1,15]]
         simulate_batch('point', fault_tolerance_args, restrictions_fault_tolerance, simulate_disturbances = True, dataset_save_path = dataset_save_path)
 
     # Save dataset

6dof_quadrotor/trajectory_handler.py
@@ -187,10 +187,10 @@ class TrajectoryHandler(object):
         return points_vector
     
     def generate_circle_xy_trajectories(self):
-        short_radius_vector = np.arange(0.5, 5, 0.5)
-        long_radius_vector = np.arange(5, 10, 0.2)
-        short_period_vector = np.arange(2, 10, 1)
-        long_period_vector = np.arange(10, 30, 1)
+        short_radius_vector = np.arange(1, 5, 1)
+        long_radius_vector = np.arange(5, 10, 1)
+        short_period_vector = np.arange(2, 8, 1)
+        long_period_vector = np.arange(10, 15, 1)
 
         w_short_vector = 2*np.pi/short_period_vector
         w_long_vector = 2*np.pi/long_period_vector
@@ -206,14 +206,14 @@ class TrajectoryHandler(object):
                 #args = np.concatenate((args, [[2*np.pi/period, radius, 1.3*period]]), axis = 0)
                 args.append([2*np.pi/period, radius, 1.25*period])
         num_circles = len(short_radius_vector) * len(short_period_vector) + len(long_radius_vector) * len(long_period_vector)
-
+        print('num_circles =',num_circles)
         return args
     
     def generate_circle_xz_trajectories(self):
         short_radius_vector = np.arange(1, 5, 1)
-        long_radius_vector = np.arange(5, 10, 1)
-        short_period_vector = np.arange(2, 8, 1)
-        long_period_vector = np.arange(10, 20, 1)
+        long_radius_vector = np.arange(5, 7, 1)
+        short_period_vector = np.arange(2, 8, 2)
+        long_period_vector = np.arange(10, 14, 2)
 
         w_short_vector = 2*np.pi/short_period_vector
         w_long_vector = 2*np.pi/long_period_vector
@@ -243,9 +243,9 @@ class TrajectoryHandler(object):
     
     def generate_lissajous_xy_trajectories(self):
         short_radius_vector = np.arange(1, 5, 1)
-        long_radius_vector = np.arange(5, 10, 1)
-        short_period_vector = np.arange(4, 8, 1)
-        long_period_vector = np.arange(10, 20, 1)
+        long_radius_vector = np.arange(5, 7, 1)
+        short_period_vector = np.arange(2, 8, 2)
+        long_period_vector = np.arange(10, 14, 2)
 
         w_short_vector = 2*np.pi/short_period_vector
         w_long_vector = 2*np.pi/long_period_vector
@@ -264,8 +264,10 @@ class TrajectoryHandler(object):
         print('lissajous_xy trajectories =',num_circles)
         return args
 
-teste = TrajectoryHandler()
-teste.generate_circle_xy_trajectories()
+if __name__ == '__main__':
+
+    teste = TrajectoryHandler()
+    teste.generate_circle_xy_trajectories()
 #args = teste.generate_line_trajectories()
 #print('argsshape',np.shape(args))
 