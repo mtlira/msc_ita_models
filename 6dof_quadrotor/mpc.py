fix RMSe formula and execution time more precise

6dof_quadrotor/mpc.py
@@ -7,6 +7,7 @@ import cvxopt
 from linearize import discretize
 import time
 from linearize import *
+from plots import DataAnalyser
 
 class MPC(object):
     def __init__(self, M, N, A, B, C, time_step, T_sample, output_weights, control_weights, restrictions, u_ref,\
@@ -341,7 +342,7 @@ class MPC(object):
         Takes into account future trajectory reference from trajectory[k] until trajectory[k+N-1]
         """
 
-        start_time = time.time()
+        analyser = DataAnalyser()
 
         p = self.p
         q = self.q
@@ -369,6 +370,9 @@ class MPC(object):
         # NN Dataset
         NN_dataset = [] if generate_dataset else None
 
+        execution_time = 0
+        waste_time = 0
+        start_time = time.perf_counter()
         for k in range(0, len(t_samples)-1): # TODO: confirmar se é -1 mesmo:
             ref_N = trajectory[k:k+self.N].reshape(-1) # TODO validar se termina em k+N-1 ou em k+N
             if np.shape(ref_N)[0] < q*self.N:
@@ -427,9 +431,12 @@ class MPC(object):
 
             # Apply INPUT disturbance if enabled
             if disturb_input and k>0:
+                waste_start_time = time.perf_counter()
                 probability = np.random.rand()
                 if probability > disturb_frequency:
-                    uu_k = self.add_input_disturbance(uu_k, model)                
+                    uu_k = self.add_input_disturbance(uu_k, model)
+                waste_end_time = time.perf_counter()
+                waste_time += waste_end_time - waste_start_time               
 
             # Apply control u_k in the multi-rotor
             f_t_k, t_x_k, t_y_k, t_z_k = uu_k # Attention for u_eq (solved the problem)
@@ -447,6 +454,9 @@ class MPC(object):
             x_k_old = x_k # Used only to mount nn_sample array
             x_k = odeint(model.f2, x_k, t_simulation, args = (f_t_k, t_x_k, t_y_k, t_z_k))
             x_k = x_k[-1]
+
+            waste_start_time= time.perf_counter()
+
             if np.linalg.norm(x_k[9:12] - trajectory[k, :3]) > 10: #or np.max(np.abs(x_k[0:2])) > 1.75:
                 print('Simulation exploded.')
                 print(f'x_{k} =',x_k)
@@ -459,9 +469,7 @@ class MPC(object):
             #delta_u_initial = np.tile(delta_u_k,self.M)
 
             # Storing dataset samples
-            waste_time = 0
             if generate_dataset:
-                start_waste_time = time.time()
                 NN_sample = np.array([])
 
                 # Calculating reference values relative to multirotor's current position at instant k
@@ -477,18 +485,20 @@ class MPC(object):
                 NN_sample = np.concatenate((NN_sample, x_k_old[0:9], ref_N_relative, self.restrictions['u_max'] + self.u_ref, u_k), axis = 0) #TODO: depois, acrescentar restrições
 
                 NN_dataset.append(NN_sample)
-                end_waste_time = time.time()
-                waste_time += end_waste_time - start_waste_time
 
-        end_time = time.time()
+            waste_end_time = time.perf_counter()
+            waste_time += waste_end_time - waste_start_time
+
+        end_time = time.perf_counter()
         
         # Metadata
         X_vector = np.array(X_vector)
         execution_time = (end_time - start_time) - waste_time
 
         position = X_vector[:, 9:]
-        delta_position = trajectory[:len(position),:3] - position
-        RMSe = np.sqrt(np.mean(delta_position**2))
+        #delta_position = trajectory[:len(position),:3] - position
+        #RMSe = np.sqrt(np.mean(delta_position**2))
+        RMSe = analyser.RMSe(position, trajectory[:len(position),:3])
 
         min_phi = np.min(X_vector[:,0])
         max_phi = np.max(X_vector[:,0])
@@ -506,22 +516,22 @@ class MPC(object):
         std_psi = np.std(X_vector[:,2])
 
         metadata = {
-            'execution_time': execution_time,
-            'RMSe': RMSe,
-            'min_phi': min_phi,
-            'max_phi': max_phi,
-            'mean_phi': mean_phi,
-            'std_phi': std_phi,
-            'min_theta': min_theta,
-            'max_theta': max_theta,
-            'mean_theta': mean_theta,
-            'std_theta': std_theta,
-            'min_psi': min_psi,
-            'max_psi': max_psi,
-            'mean_psi': mean_psi,
-            'std_psi': std_psi,
+            'num_iterations': len(t_samples)-1,    
+            'mpc_execution_time': execution_time,
+            'mpc_RMSe': RMSe,
+            'mpc_min_phi': min_phi,
+            'mpc_max_phi': max_phi,
+            'mpc_mean_phi': mean_phi,
+            'mpc_std_phi': std_phi,
+            'mpc_min_theta': min_theta,
+            'mpc_max_theta': max_theta,
+            'mpc_mean_theta': mean_theta,
+            'mpc_std_theta': std_theta,
+            'mpc_min_psi': min_psi,
+            'mpc_max_psi': max_psi,
+            'mpc_mean_psi': mean_psi,
+            'mpc_std_psi': std_psi,
         }
-
 
         return np.array(X_vector), np.array(u_vector), np.array(omega_vector), np.asarray(NN_dataset), metadata
     
@@ -970,8 +980,7 @@ class GainSchedulingMPC(object):
         Takes into account future trajectory reference from trajectory[k] until trajectory[k+N-1]\n
         Gets the linear model closest to the current operating point regarding pitch and roll angles at every iteration
         """
-
-        start_time = time.time()
+        analyser = DataAnalyser()
 
         #p = len(self.linear_model[(0,0)].u_ref)
         #q = len(self.linear_model[(0,0)].C)
@@ -995,6 +1004,10 @@ class GainSchedulingMPC(object):
         # NN Dataset
         NN_dataset = [] if generate_dataset else None
         #print('q = ',q)
+
+        execution_time = 0
+        waste_time = 0
+        start_time = time.perf_counter()
         for k in range(0, len(t_samples)-1): # TODO: confirmar se é -1 mesmo:
             linear_model = self.choose_model(x_k[0], x_k[1])
             ref_N = trajectory[k:k+self.N].reshape(-1) # TODO validar se termina em k+N-1 ou em k+N
@@ -1056,9 +1069,12 @@ class GainSchedulingMPC(object):
 
             # Apply INPUT disturbance if enabled
             if disturb_input and k>0:
+                waste_start_time = time.perf_counter()
                 probability = np.random.rand()
                 if probability > disturb_frequency:
-                    uu_k = linear_model.add_input_disturbance(uu_k, model)                
+                    uu_k = linear_model.add_input_disturbance(uu_k, model)
+                waste_end_time = time.perf_counter()
+                waste_time += waste_end_time - waste_start_time
 
             # Apply control u_k in the multi-rotor
             f_t_k, t_x_k, t_y_k, t_z_k = uu_k # Attention for u_eq (solved the problem)
@@ -1076,11 +1092,14 @@ class GainSchedulingMPC(object):
             x_k_old = x_k # Used only to mount nn_sample array
             x_k = odeint(model.f2, x_k, t_simulation, args = (f_t_k, t_x_k, t_y_k, t_z_k))
             x_k = x_k[-1]
+
+            waste_start_time = time.perf_counter()
+
             if np.linalg.norm(x_k[9:12] - trajectory[k, :3]) > 10:# or np.max(np.abs(x_k[0:2])) > 2.5: #TODO: unificar criterio de saida para todos os metodos
                 print('Simulation exploded.')
                 print(f'x_{k} =',x_k)
                 return None, None, None, None, None
-            
+                  
             X_vector.append(x_k)
             u_k_minus_1 = u_k
             u_vector.append(uu_k)
@@ -1088,9 +1107,7 @@ class GainSchedulingMPC(object):
             #delta_u_initial = np.tile(delta_u_k,self.M)
 
             # Storing dataset samples
-            waste_time = 0
             if generate_dataset:
-                start_waste_time = time.time()
                 NN_sample = np.array([])
 
                 # Calculating reference values relative to multirotor's current position at instant k
@@ -1106,19 +1123,20 @@ class GainSchedulingMPC(object):
                 NN_sample = np.concatenate((NN_sample, x_k_old[0:9], ref_N_relative, linear_model.restrictions['u_max'] + linear_model.u_ref, u_k), axis = 0) #TODO: depois, acrescentar restrições
 
                 NN_dataset.append(NN_sample)
-                end_waste_time = time.time()
-                waste_time += end_waste_time - start_waste_time
 
-        end_time = time.time()
+            waste_end_time = time.perf_counter()
+            waste_time += waste_end_time - waste_start_time
+
+        end_time = time.perf_counter()
         
         # Metadata
         X_vector = np.array(X_vector)
         execution_time = (end_time - start_time) - waste_time
 
         position = X_vector[:, 9:]
-        delta_position = trajectory[:len(position),:3] - position
-        RMSe = np.sqrt(np.mean(delta_position**2))
-        #RMSe = linear_model.RMSe(position, trajectory[:len(position),:3])
+        #delta_position = trajectory[:len(position),:3] - position
+        #RMSe = np.sqrt(np.mean(delta_position**2))
+        RMSe = analyser.RMSe(position, trajectory[:len(position),:3])
 
         #print('RMSe',RMSe, '\nWrong RMSe',RMSe_wrong)
 
@@ -1138,20 +1156,21 @@ class GainSchedulingMPC(object):
         std_psi = np.std(X_vector[:,2])
 
         metadata = {
-            'execution_time': execution_time,
-            'RMSe': RMSe,
-            'min_phi': min_phi,
-            'max_phi': max_phi,
-            'mean_phi': mean_phi,
-            'std_phi': std_phi,
-            'min_theta': min_theta,
-            'max_theta': max_theta,
-            'mean_theta': mean_theta,
-            'std_theta': std_theta,
-            'min_psi': min_psi,
-            'max_psi': max_psi,
-            'mean_psi': mean_psi,
-            'std_psi': std_psi,
+            'num_iterations': len(t_samples)-1,
+            'mpc_execution_time': execution_time,
+            'mpc_RMSe': RMSe,
+            'mpc_min_phi': min_phi,
+            'mpc_max_phi': max_phi,
+            'mpc_mean_phi': mean_phi,
+            'mpc_std_phi': std_phi,
+            'mpc_min_theta': min_theta,
+            'mpc_max_theta': max_theta,
+            'mpc_mean_theta': mean_theta,
+            'mpc_std_theta': std_theta,
+            'mpc_min_psi': min_psi,
+            'mpc_max_psi': max_psi,
+            'mpc_mean_psi': mean_psi,
+            'mpc_std_psi': std_psi,
         }
 
 

6dof_quadrotor/plots.py
@@ -1,241 +1,267 @@
 import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
 import numpy as np
+import os
+import pandas as pd
 
-def plot_states(X,t, X_lin = None, trajectory = None, u_vector = None, omega_vector = None, equal_scales=False, legend = [], save_path = None):
-    handles = []
-    
-    # Rotation
-    fig, axs = plt.subplots(2, 3)
-    axs[0,0].plot(t,X[:,0])
-    if X_lin is not None: axs[0,0].plot(t,X_lin[:,0])
-    if trajectory is not None: axs[0,2].plot(np.nan, np.nan, 'g--')
-    axs[0,0].set_title('$\\phi(t)$')
-    axs[0,0].set_xlabel('t (s)')
-    axs[0,0].set_ylabel('$\\phi (rad)$')
+class DataAnalyser(object):
+    def __init__(self):
+        self.dataset = None
 
-    axs[0,1].plot(t,(-1)*X[:,1])
-    if X_lin is not None: axs[0,1].plot(t,(-1)*X_lin[:,1])
-    if trajectory is not None: axs[0,2].plot(np.nan, np.nan, 'g--')
-    axs[0,1].set_title('$\\theta(t)$')
-    axs[0,1].set_xlabel('t (s)')
-    axs[0,1].set_ylabel('$\\theta (rad)$')
-
-    axs[0,2].plot(t,(-1)*X[:,2])
-    if X_lin is not None: axs[0,2].plot(t,(-1)*X_lin[:,2])
-    if trajectory is not None: axs[0,2].plot(np.nan, np.nan, 'g--')
-    axs[0,2].set_title('$\\psi(t)$')
-    axs[0,2].set_xlabel('t (s)')
-    axs[0,2].set_ylabel('$\\psi$ (rad)')
-
-    axs[1,0].plot(t,X[:,3])
-    if X_lin is not None: axs[1,0].plot(t,X_lin[:,3])
-    if trajectory is not None: axs[0,2].plot(np.nan, np.nan, 'g--')
-    axs[1,0].set_title('p(t)')
-    axs[1,0].set_xlabel('t (s)')
-    axs[1,0].set_ylabel('p (rad/s)')
-
-    axs[1,1].plot(t,(-1)*X[:,4])
-    if X_lin is not None: axs[1,1].plot(t,(-1)*X_lin[:,4])
-    if trajectory is not None: axs[0,2].plot(np.nan, np.nan, 'g--')
-    axs[1,1].set_title('q(t)')
-    axs[1,1].set_xlabel('t (s)')
-    axs[1,1].set_ylabel('q (rad/s)')
-
-    axs[1,2].plot(t,(-1)*X[:,5])
-    if X_lin is not None: axs[1,2].plot(t,(-1)*X_lin[:,5])
-    if trajectory is not None: axs[0,2].plot(np.nan, np.nan, 'g--')
-    axs[1,2].set_title('r(t)')
-    axs[1,2].set_xlabel('t (s)')
-    axs[1,2].set_ylabel('r (rad/s)')
-    fig.legend(legend if trajectory is None else legend[:-1]) # Because there is no reference in angle values
-    plt.subplots_adjust(left=0.083, bottom=0.083, right=0.948, top=0.914, wspace=0.23, hspace=0.31)
-    
-    if save_path is not None: 
-        plt.savefig(save_path + 'x_angular.png')
-        for ax in axs.reshape(-1): ax.cla()
-        plt.close(fig)
-        del fig
-        del axs
-
-    # Translation
-    fig, axs = plt.subplots(2, 3)
-    axs[0,0].plot(t,X[:,6])
-    if X_lin is not None: axs[0,0].plot(t,X_lin[:,6])
-    if trajectory is not None: axs[0,2].plot(np.nan, np.nan, 'g--')
-    axs[0,0].set_title('u(t)')
-    axs[0,0].set_xlabel('t (s)')
-    axs[0,0].set_ylabel('u (m/s)')
-
-    axs[0,1].plot(t,(-1)*X[:,7])
-    if X_lin is not None: axs[0,1].plot(t,(-1)*X_lin[:,7])
-    if trajectory is not None: axs[0,2].plot(np.nan, np.nan, 'g--')
-    axs[0,1].set_title('v(t)')
-    axs[0,1].set_xlabel('t (s)')
-    axs[0,1].set_ylabel('v (m/s)')
-
-    axs[0,2].plot(t,(-1)*X[:,8])
-    if X_lin is not None: axs[0,2].plot(t,(-1)*X_lin[:,8])
-    if trajectory is not None: axs[0,2].plot(np.nan, np.nan, 'g--')
-    axs[0,2].set_title('w(t)')
-    axs[0,2].set_xlabel('t (s)')
-    axs[0,2].set_ylabel('w (m/s)')
-
-    handles.append(axs[1,0].plot(t,X[:,9])[0])
-    if X_lin is not None: handles.append(axs[1,0].plot(t,X_lin[:,9])[0])
-    if trajectory is not None: handles.append(axs[1,0].plot(t,trajectory[:,0], 'g--')[0])
-    axs[1,0].set_title('x(t)')
-    axs[1,0].set_xlabel('t (s)')
-    axs[1,0].set_ylabel('x (m)')
-
-    axs[1,1].plot(t,(-1)*X[:,10])
-    if X_lin is not None: axs[1,1].plot(t,(-1)*X_lin[:,10])
-    if trajectory is not None: axs[1,1].plot(t,-trajectory[:,1], 'g--')
-    axs[1,1].set_title('y(t)')
-    axs[1,1].set_xlabel('t (s)')
-    axs[1,1].set_ylabel('y (m)')
-
-    axs[1,2].plot(t,(-1)*X[:,11])
-    if X_lin is not None: axs[1,2].plot(t,(-1)*X_lin[:,11])
-    if trajectory is not None: axs[1,2].plot(t,-trajectory[:,2], 'g--')
-    axs[1,2].set_title('z(t)')
-    axs[1,2].set_xlabel('t (s)')
-    axs[1,2].set_ylabel('z (m)')
-
-    for i in range(len(handles)):
-        handles[i] = mlines.Line2D([], [],
-                                 color=handles[i].get_color(),
-                                 linestyle=handles[i].get_linestyle(),
-                                 label=f'c{i+1}' if i >= len(legend) else legend[i])
-    
-    fig.legend(handles=handles)
-    plt.subplots_adjust(left=0.083, bottom=0.083, right=0.948, top=0.914, wspace=0.23, hspace=0.31)
-
-
-    if save_path is not None: 
-        plt.savefig(save_path + 'x_linear.png')
-        for ax in axs.reshape(-1): ax.cla()
-        plt.close(fig)
-        del fig
-        del axs
-
-    fig = plt.figure()
-    axs = plt.axes(projection='3d')
-    axs.plot3D(X[:,9], X[:,10]*(-1), X[:,11]*(-1))
-    if X_lin is not None: axs.plot3D(X_lin[:,9], X_lin[:,10]*(-1), X_lin[:,11]*(-1))
-    if trajectory is not None: axs.plot3D(trajectory[:,0], -trajectory[:,1], -trajectory[:,2], 'g--')
-    axs.set_xlabel('x (m)')
-    axs.set_ylabel('y (m)')
-    axs.set_zlabel('z (m)')
-    axs.set_title('3D Plot')
-    fig.legend(handles=handles)
-    if equal_scales: axs.set_aspect('equal', adjustable='box')
-
-
-    if save_path is not None: 
-        plt.savefig(save_path + '3D.png')
-        axs.cla()
-        plt.close(fig)
-        del fig
-        del axs
-
-    plot_inputs(u_vector, t, omega_vector, save_path)
-    if save_path is None: plt.show()
-    plt.close('all')
-
-def plot_inputs(u_vector, t, omega_vector = None, save_path=None):
-    t = t[0:-1]
-    fig, axs = plt.subplots(2, 2)
-    axs[0,0].step(t,u_vector[:,0])
-    axs[0,0].set_title('f_t (t)')
-    axs[0,0].set_ylabel('f_t (N)')
-    axs[0,0].set_xlabel('t (s)')
-
-    axs[0,1].step(t,u_vector[:,1])
-    axs[0,1].set_title('$\\tau_x (t)$')
-    axs[0,1].set_ylabel('$\\tau_x (N.m)$')
-    axs[0,1].set_xlabel('t (s)')
-
-    axs[1,0].step(t,u_vector[:,2])
-    axs[1,0].set_title('$\\tau_y (t)$')
-    axs[1,0].set_ylabel('$\\tau_y (N.m)$')
-    axs[1,0].set_xlabel('t (s)')
-
-    axs[1,1].step(t,u_vector[:,3])
-    axs[1,1].set_title('$\\tau_z (t)$')
-    axs[1,1].set_ylabel('$\\tau_z (N.m)$')
-    axs[1,1].set_xlabel('t (s)')
-    plt.subplots_adjust(left=0.125, bottom=0.071, right=0.921, top=0.96, wspace=0.195, hspace=0.279)
-
-
-    if save_path is not None: 
-        plt.savefig(save_path + 'inputs-forces.png')
-        for ax in axs.reshape(-1): ax.cla()
-        plt.close(fig)
-        del fig
-        del axs
-
-    if omega_vector is not None:
-        fig, axs = plt.subplots(2, 2)
-        axs[0,0].step(t,omega_vector[:,0])
-        axs[0,0].set_title('$\\omega_1 (t)$')
-        axs[0,0].set_ylabel('$\\omega_1 (rad/s)$')
-        axs[0,0].set_xlabel('t (s)')
-
-        axs[0,1].step(t,omega_vector[:,1])
-        axs[0,1].set_title('$\\omega_2 (t)$')
-        axs[0,1].set_ylabel('$\\omega_2 (rad/s)$')
-        axs[0,1].set_xlabel('t (s)')
-
-        axs[1,0].step(t,omega_vector[:,2])
-        axs[1,0].set_title('$\\omega_3 (t)$')
-        axs[1,0].set_ylabel('$\\omega_3 (rad/s)$')
-        axs[1,0].set_xlabel('t (s)')
-
-        axs[1,1].step(t,omega_vector[:,3])
-        axs[1,1].set_title('$\\omega_4 (t)$')
-        axs[1,1].set_ylabel('$\\omega_4 (rad/s)$')
-        axs[1,1].set_xlabel('t (s)')
-        plt.subplots_adjust(left=0.125, bottom=0.071, right=0.921, top=0.96, wspace=0.195, hspace=0.279)
+    def plot_states(self, X,t, X_lin = None, trajectory = None, u_vector = None, omega_vector = None, equal_scales=False, legend = [], save_path = None):
+        handles = []
         
+        # Rotation
+        fig, axs = plt.subplots(2, 3)
+        axs[0,0].plot(t,X[:,0])
+        if X_lin is not None: axs[0,0].plot(t,X_lin[:,0])
+        if trajectory is not None: axs[0,2].plot(np.nan, np.nan, 'g--')
+        axs[0,0].set_title('$\\phi(t)$')
+        axs[0,0].set_xlabel('t (s)')
+        axs[0,0].set_ylabel('$\\phi (rad)$')
 
+        axs[0,1].plot(t,(-1)*X[:,1])
+        if X_lin is not None: axs[0,1].plot(t,(-1)*X_lin[:,1])
+        if trajectory is not None: axs[0,2].plot(np.nan, np.nan, 'g--')
+        axs[0,1].set_title('$\\theta(t)$')
+        axs[0,1].set_xlabel('t (s)')
+        axs[0,1].set_ylabel('$\\theta (rad)$')
+
+        axs[0,2].plot(t,(-1)*X[:,2])
+        if X_lin is not None: axs[0,2].plot(t,(-1)*X_lin[:,2])
+        if trajectory is not None: axs[0,2].plot(np.nan, np.nan, 'g--')
+        axs[0,2].set_title('$\\psi(t)$')
+        axs[0,2].set_xlabel('t (s)')
+        axs[0,2].set_ylabel('$\\psi$ (rad)')
+
+        axs[1,0].plot(t,X[:,3])
+        if X_lin is not None: axs[1,0].plot(t,X_lin[:,3])
+        if trajectory is not None: axs[0,2].plot(np.nan, np.nan, 'g--')
+        axs[1,0].set_title('p(t)')
+        axs[1,0].set_xlabel('t (s)')
+        axs[1,0].set_ylabel('p (rad/s)')
+
+        axs[1,1].plot(t,(-1)*X[:,4])
+        if X_lin is not None: axs[1,1].plot(t,(-1)*X_lin[:,4])
+        if trajectory is not None: axs[0,2].plot(np.nan, np.nan, 'g--')
+        axs[1,1].set_title('q(t)')
+        axs[1,1].set_xlabel('t (s)')
+        axs[1,1].set_ylabel('q (rad/s)')
+
+        axs[1,2].plot(t,(-1)*X[:,5])
+        if X_lin is not None: axs[1,2].plot(t,(-1)*X_lin[:,5])
+        if trajectory is not None: axs[0,2].plot(np.nan, np.nan, 'g--')
+        axs[1,2].set_title('r(t)')
+        axs[1,2].set_xlabel('t (s)')
+        axs[1,2].set_ylabel('r (rad/s)')
+        fig.legend(legend if trajectory is None else legend[:-1]) # Because there is no reference in angle values
+        plt.subplots_adjust(left=0.083, bottom=0.083, right=0.948, top=0.914, wspace=0.23, hspace=0.31)
+        
         if save_path is not None: 
-            plt.savefig(save_path + 'inputs-rotors1.png')
+            plt.savefig(save_path + 'x_angular.png')
             for ax in axs.reshape(-1): ax.cla()
             plt.close(fig)
             del fig
             del axs
 
-        if np.shape(omega_vector)[1] == 8:
+        # Translation
+        fig, axs = plt.subplots(2, 3)
+        axs[0,0].plot(t,X[:,6])
+        if X_lin is not None: axs[0,0].plot(t,X_lin[:,6])
+        if trajectory is not None: axs[0,2].plot(np.nan, np.nan, 'g--')
+        axs[0,0].set_title('u(t)')
+        axs[0,0].set_xlabel('t (s)')
+        axs[0,0].set_ylabel('u (m/s)')
+
+        axs[0,1].plot(t,(-1)*X[:,7])
+        if X_lin is not None: axs[0,1].plot(t,(-1)*X_lin[:,7])
+        if trajectory is not None: axs[0,2].plot(np.nan, np.nan, 'g--')
+        axs[0,1].set_title('v(t)')
+        axs[0,1].set_xlabel('t (s)')
+        axs[0,1].set_ylabel('v (m/s)')
+
+        axs[0,2].plot(t,(-1)*X[:,8])
+        if X_lin is not None: axs[0,2].plot(t,(-1)*X_lin[:,8])
+        if trajectory is not None: axs[0,2].plot(np.nan, np.nan, 'g--')
+        axs[0,2].set_title('w(t)')
+        axs[0,2].set_xlabel('t (s)')
+        axs[0,2].set_ylabel('w (m/s)')
+
+        handles.append(axs[1,0].plot(t,X[:,9])[0])
+        if X_lin is not None: handles.append(axs[1,0].plot(t,X_lin[:,9])[0])
+        if trajectory is not None: handles.append(axs[1,0].plot(t,trajectory[:,0], 'g--')[0])
+        axs[1,0].set_title('x(t)')
+        axs[1,0].set_xlabel('t (s)')
+        axs[1,0].set_ylabel('x (m)')
+
+        axs[1,1].plot(t,(-1)*X[:,10])
+        if X_lin is not None: axs[1,1].plot(t,(-1)*X_lin[:,10])
+        if trajectory is not None: axs[1,1].plot(t,-trajectory[:,1], 'g--')
+        axs[1,1].set_title('y(t)')
+        axs[1,1].set_xlabel('t (s)')
+        axs[1,1].set_ylabel('y (m)')
+
+        axs[1,2].plot(t,(-1)*X[:,11])
+        if X_lin is not None: axs[1,2].plot(t,(-1)*X_lin[:,11])
+        if trajectory is not None: axs[1,2].plot(t,-trajectory[:,2], 'g--')
+        axs[1,2].set_title('z(t)')
+        axs[1,2].set_xlabel('t (s)')
+        axs[1,2].set_ylabel('z (m)')
+
+        for i in range(len(handles)):
+            handles[i] = mlines.Line2D([], [],
+                                    color=handles[i].get_color(),
+                                    linestyle=handles[i].get_linestyle(),
+                                    label=f'c{i+1}' if i >= len(legend) else legend[i])
+        
+        fig.legend(handles=handles)
+        plt.subplots_adjust(left=0.083, bottom=0.083, right=0.948, top=0.914, wspace=0.23, hspace=0.31)
+
+
+        if save_path is not None: 
+            plt.savefig(save_path + 'x_linear.png')
+            for ax in axs.reshape(-1): ax.cla()
+            plt.close(fig)
+            del fig
+            del axs
+
+        fig = plt.figure()
+        axs = plt.axes(projection='3d')
+        axs.plot3D(X[:,9], X[:,10]*(-1), X[:,11]*(-1))
+        if X_lin is not None: axs.plot3D(X_lin[:,9], X_lin[:,10]*(-1), X_lin[:,11]*(-1))
+        if trajectory is not None: axs.plot3D(trajectory[:,0], -trajectory[:,1], -trajectory[:,2], 'g--')
+        axs.set_xlabel('x (m)')
+        axs.set_ylabel('y (m)')
+        axs.set_zlabel('z (m)')
+        axs.set_title('3D Plot')
+        fig.legend(handles=handles)
+        if equal_scales: axs.set_aspect('equal', adjustable='box')
+
+
+        if save_path is not None: 
+            plt.savefig(save_path + '3D.png')
+            axs.cla()
+            plt.close(fig)
+            del fig
+            del axs
+
+        self.plot_inputs(u_vector, t, omega_vector, save_path)
+        if save_path is None: plt.show()
+        plt.close('all')
+
+    def plot_inputs(self, u_vector, t, omega_vector = None, save_path=None):
+        t = t[0:-1]
+        fig, axs = plt.subplots(2, 2)
+        axs[0,0].step(t,u_vector[:,0])
+        axs[0,0].set_title('f_t (t)')
+        axs[0,0].set_ylabel('f_t (N)')
+        axs[0,0].set_xlabel('t (s)')
+
+        axs[0,1].step(t,u_vector[:,1])
+        axs[0,1].set_title('$\\tau_x (t)$')
+        axs[0,1].set_ylabel('$\\tau_x (N.m)$')
+        axs[0,1].set_xlabel('t (s)')
+
+        axs[1,0].step(t,u_vector[:,2])
+        axs[1,0].set_title('$\\tau_y (t)$')
+        axs[1,0].set_ylabel('$\\tau_y (N.m)$')
+        axs[1,0].set_xlabel('t (s)')
+
+        axs[1,1].step(t,u_vector[:,3])
+        axs[1,1].set_title('$\\tau_z (t)$')
+        axs[1,1].set_ylabel('$\\tau_z (N.m)$')
+        axs[1,1].set_xlabel('t (s)')
+        plt.subplots_adjust(left=0.125, bottom=0.071, right=0.921, top=0.96, wspace=0.195, hspace=0.279)
+
+
+        if save_path is not None: 
+            plt.savefig(save_path + 'inputs-forces.png')
+            for ax in axs.reshape(-1): ax.cla()
+            plt.close(fig)
+            del fig
+            del axs
+
+        if omega_vector is not None:
             fig, axs = plt.subplots(2, 2)
-            axs[0,0].step(t,omega_vector[:,4])
-            axs[0,0].set_title('$\\omega_5 (t)$')
-            axs[0,0].set_ylabel('$\\omega_5 (rad/s)$')
+            axs[0,0].step(t,omega_vector[:,0])
+            axs[0,0].set_title('$\\omega_1 (t)$')
+            axs[0,0].set_ylabel('$\\omega_1 (rad/s)$')
             axs[0,0].set_xlabel('t (s)')
 
-            axs[0,1].step(t,omega_vector[:,5])
-            axs[0,1].set_title('$\\omega_6 (t)$')
-            axs[0,1].set_ylabel('$\\omega_6 (rad/s)$')
+            axs[0,1].step(t,omega_vector[:,1])
+            axs[0,1].set_title('$\\omega_2 (t)$')
+            axs[0,1].set_ylabel('$\\omega_2 (rad/s)$')
             axs[0,1].set_xlabel('t (s)')
 
-            axs[1,0].step(t,omega_vector[:,6])
-            axs[1,0].set_title('$\\omega_7 (t)$')
-            axs[1,0].set_ylabel('$\\omega_7 (rad/s)$')
+            axs[1,0].step(t,omega_vector[:,2])
+            axs[1,0].set_title('$\\omega_3 (t)$')
+            axs[1,0].set_ylabel('$\\omega_3 (rad/s)$')
             axs[1,0].set_xlabel('t (s)')
 
-            axs[1,1].step(t,omega_vector[:,7])
-            axs[1,1].set_title('$\\omega_8 (t)$')
-            axs[1,1].set_ylabel('$\\omega_8 (rad/s)$')
+            axs[1,1].step(t,omega_vector[:,3])
+            axs[1,1].set_title('$\\omega_4 (t)$')
+            axs[1,1].set_ylabel('$\\omega_4 (rad/s)$')
             axs[1,1].set_xlabel('t (s)')
             plt.subplots_adjust(left=0.125, bottom=0.071, right=0.921, top=0.96, wspace=0.195, hspace=0.279)
-
+            
 
             if save_path is not None: 
-                plt.savefig(save_path + 'inputs-rotors2.png')
+                plt.savefig(save_path + 'inputs-rotors1.png')
                 for ax in axs.reshape(-1): ax.cla()
                 plt.close(fig)
                 del fig
                 del axs
+
+            if np.shape(omega_vector)[1] == 8:
+                fig, axs = plt.subplots(2, 2)
+                axs[0,0].step(t,omega_vector[:,4])
+                axs[0,0].set_title('$\\omega_5 (t)$')
+                axs[0,0].set_ylabel('$\\omega_5 (rad/s)$')
+                axs[0,0].set_xlabel('t (s)')
+
+                axs[0,1].step(t,omega_vector[:,5])
+                axs[0,1].set_title('$\\omega_6 (t)$')
+                axs[0,1].set_ylabel('$\\omega_6 (rad/s)$')
+                axs[0,1].set_xlabel('t (s)')
+
+                axs[1,0].step(t,omega_vector[:,6])
+                axs[1,0].set_title('$\\omega_7 (t)$')
+                axs[1,0].set_ylabel('$\\omega_7 (rad/s)$')
+                axs[1,0].set_xlabel('t (s)')
+
+                axs[1,1].step(t,omega_vector[:,7])
+                axs[1,1].set_title('$\\omega_8 (t)$')
+                axs[1,1].set_ylabel('$\\omega_8 (rad/s)$')
+                axs[1,1].set_xlabel('t (s)')
+                plt.subplots_adjust(left=0.125, bottom=0.071, right=0.921, top=0.96, wspace=0.195, hspace=0.279)
+
+
+                if save_path is not None: 
+                    plt.savefig(save_path + 'inputs-rotors2.png')
+                    for ax in axs.reshape(-1): ax.cla()
+                    plt.close(fig)
+                    del fig
+                    del axs
+
+    def load_datasets(self, mother_folder_path):
+        for subdir, _, files in os.walk(mother_folder_path):
+            for file in files:
+                if file == 'dataset_metadata.csv':
+                    df = pd.read_csv(mother_folder_path + file, sep=',')
+                    self.dataset = df if self.dataset is None else pd.concat([self.dataset, df])
+        self.dataset.to_csv(mother_folder_path, sep='csv', index=False)
+                
+    def plot_rmse_histogram(self, mother_folder_path):
+        pass#if self.dataset is None: self.dataset = self.load_datasets(mother_folder_path)
+
+        
+    def RMSe(self, position, trajectory):
+        '''position and trajectory dimesnions: (N_iterations, 3)'''
+        delta_position = trajectory - position   # shape (N, 3)
+        squared_norms = np.sum(delta_position**2, axis=1)  # Sum over x, y, z for each time step
+        RMSe = np.sqrt(np.mean(squared_norms))
+        return RMSe
+
 
 def plot_delays(X_nonlinear, trajectory, t, X_linear = False):
     samples_indexes = np.rint(np.linspace(0,1,11)*(len(t)-1)).astype('int')

6dof_quadrotor/simulate_mpc.py
@@ -43,6 +43,7 @@ X0 = np.array([0,0,0,0,0,0,0,0,0,0,0,0])
 A, B, C = model.linearize()
 
 tr = trajectory_handler.TrajectoryHandler()
+analyser = DataAnalyser()
 
 # Parameters
 failed_simulations = 0
@@ -76,9 +77,10 @@ simulation_metadata = {
     'mean_psi (rad)': [],
     'std_psi (rad)': [],
 }
+simulation_metadata = {}
 dataset_dataframe = pd.DataFrame(simulation_metadata)
-dataset_dataframe['success'] = dataset_dataframe['success'].astype(bool)
-dataset_dataframe['disturbed_inputs'] = dataset_dataframe['disturbed_inputs'].astype(bool)
+#dataset_dataframe['success'] = dataset_dataframe['success'].astype(bool)
+#dataset_dataframe['disturbed_inputs'] = dataset_dataframe['disturbed_inputs'].astype(bool)
 
 # MPC Implementation
 
@@ -125,7 +127,7 @@ def simulate_mpc(X0, time_step, T_sample, T_simulation, trajectory, restrictions
             Path(save_path).mkdir(parents=True, exist_ok=True)
             #np.savetxt(save_path + "dataset.csv", NN_dataset, delimiter=",")
             np.save(save_path + 'dataset.npy', NN_dataset.astype(np.float32))
-        plot_states(x_mpc_rotors, t_samples[:np.shape(x_mpc_rotors)[0]], trajectory=trajectory, u_vector=u_rotors, omega_vector=omega_vector, equal_scales=True, legend=['MPC', 'Trajectory'], save_path=save_path)
+        analyser.plot_states(x_mpc_rotors, t_samples[:np.shape(x_mpc_rotors)[0]], trajectory=trajectory, u_vector=u_rotors, omega_vector=omega_vector, equal_scales=True, legend=['MPC', 'Trajectory'], save_path=save_path)
         return True, metadata, simulation_data
     return False, None, None
 
@@ -159,34 +161,8 @@ def simulate_batch(trajectory_type, args_vector, restrictions_vector, simulate_d
                 #    output_weights_hover[3:5] *= 4 # Divide delta_y_max by 2 for phi and theta
                 #    simulation_success, simulation_metadata, _ = simulate_mpc(X0, time_step, T_sample, T_simulation, trajectory, restrictions, output_weights_hover, control_weights, dataset_name, folder_name, disturb_input = False, gain_scheduling=True)
 
-                simulation_metadata = {
-                    'sim_id': dataset_id,
-                    'trajectory_type': trajectory_type,
-                    'disturbed_inputs': False,
-                    'simulation_time (s)': T_simulation,
-                    'time_sample (s)': T_sample,
-                    'N': N,
-                    'M': M,
-                    'success': simulation_success,
-                    'RMSe': simulation_metadata['RMSe'] if simulation_success else 'nan',
-                    'execution_time (s)': simulation_metadata['execution_time'] if simulation_success else 'nan',
-                    'operation': restrictions_metadata['operation'],
-                    'failed_rotors': restrictions_metadata['failed_rotors'],
-                    'ang_speed_percentages (%)': restrictions_metadata['ang_speed_percentages'],
-                    'min_phi (rad)': simulation_metadata['min_phi'] if simulation_success else 'nan',
-                    'max_phi (rad)': simulation_metadata['max_phi'] if simulation_success else 'nan',
-                    'mean_phi (rad)': simulation_metadata['mean_phi'] if simulation_success else 'nan',
-                    'std_phi (rad)': simulation_metadata['std_phi'] if simulation_success else 'nan',
-                    'min_theta (rad)': simulation_metadata['min_theta'] if simulation_success else 'nan',
-                    'max_theta (rad)': simulation_metadata['max_theta'] if simulation_success else 'nan',
-                    'mean_theta (rad)': simulation_metadata['mean_theta'] if simulation_success else 'nan',
-                    'std_theta (rad)': simulation_metadata['std_theta'] if simulation_success else 'nan',
-                    'min_psi (rad)': simulation_metadata['min_psi'] if simulation_success else 'nan',
-                    'max_psi (rad)': simulation_metadata['max_psi'] if simulation_success else 'nan',
-                    'mean_psi (rad)': simulation_metadata['mean_psi'] if simulation_success else 'nan',
-                    'std_psi (rad)': simulation_metadata['std_psi'] if simulation_success else 'nan',
-                }
-                simulation_metadata = pd.DataFrame([simulation_metadata])
+                simulation_metadata = wrap_metadata(dataset_id, trajectory_type, T_simulation, T_sample, N, M, simulation_success, \
+                                                    simulation_metadata, restrictions_metadata, False)
 
                 if not simulation_success: failed_simulations += 1
                 dataset_id += 1
@@ -209,34 +185,8 @@ def simulate_batch(trajectory_type, args_vector, restrictions_vector, simulate_d
                     #    simulation_success, simulation_metadata = simulate_mpc(X0, time_step, T_sample, T_simulation, trajectory, restrictions, output_weights_hover, control_weights, dataset_name, folder_name, disturb_input = True)
 
 
-                    simulation_metadata = {
-                        'sim_id': dataset_id,
-                        'trajectory_type': trajectory_type,
-                        'disturbed_inputs': True,
-                        'simulation_time (s)': T_simulation,
-                        'time_sample (s)': T_sample,
-                        'N': N,
-                        'M': M,
-                        'success': simulation_success,
-                        'RMSe': simulation_metadata['RMSe'] if simulation_success else 'nan',
-                        'execution_time (s)': simulation_metadata['execution_time'] if simulation_success else 'nan',
-                        'operation': restrictions_metadata['operation'],
-                        'failed_rotors': restrictions_metadata['failed_rotors'],
-                        'ang_speed_percentages (%)': restrictions_metadata['ang_speed_percentages'],
-                        'min_phi (rad)': simulation_metadata['min_phi'] if simulation_success else 'nan',
-                        'max_phi (rad)': simulation_metadata['max_phi'] if simulation_success else 'nan',
-                        'mean_phi (rad)': simulation_metadata['mean_phi'] if simulation_success else 'nan',
-                        'std_phi (rad)': simulation_metadata['std_phi'] if simulation_success else 'nan',
-                        'min_theta (rad)': simulation_metadata['min_theta'] if simulation_success else 'nan',
-                        'max_theta (rad)': simulation_metadata['max_theta'] if simulation_success else 'nan',
-                        'mean_theta (rad)': simulation_metadata['mean_theta'] if simulation_success else 'nan',
-                        'std_theta (rad)': simulation_metadata['std_theta'] if simulation_success else 'nan',
-                        'min_psi (rad)': simulation_metadata['min_psi'] if simulation_success else 'nan',
-                        'max_psi (rad)': simulation_metadata['max_psi'] if simulation_success else 'nan',
-                        'mean_psi (rad)': simulation_metadata['mean_psi'] if simulation_success else 'nan',
-                        'std_psi (rad)': simulation_metadata['std_psi'] if simulation_success else 'nan',
-                    }
-                    simulation_metadata = pd.DataFrame([simulation_metadata])
+                    simulation_metadata = wrap_metadata(dataset_id, trajectory_type, T_simulation, T_sample, N, M, simulation_success, simulation_metadata, \
+                        restrictions_metadata, True)
 
                     if not simulation_success: failed_simulations += 1
                     dataset_id += 1
@@ -302,6 +252,39 @@ def generate_dataset(dataset_name = None):
         dataset_dataframe.to_csv(dataset_save_path, sep=',', index = False)
     
     print(f'Failed simulations: {failed_simulations}/{total_simulations}') # TODO: deixar mais generico (nao so pontos)
+
+def wrap_metadata(dataset_id, trajectory_type, T_simulation, T_sample, N, M, simulation_success, simulation_metadata, \
+                  restrictions_metadata, disturbed_inputs):
+    simulation_metadata = {
+        'sim_id': dataset_id,
+        'trajectory_type': trajectory_type,
+        'disturbed_inputs': disturbed_inputs,
+        'simulation_time (s)': T_simulation,
+        'time_sample (s)': T_sample,
+        'N': N,
+        'M': M,
+        'mpc_success': simulation_success,
+        'num_iterations': simulation_metadata['num_iterations'] if simulation_success else 'nan',   
+        'mpc_RMSe': simulation_metadata['mpc_RMSe'] if simulation_success else 'nan',
+        'mpc_execution_time (s)': simulation_metadata['mpc_execution_time'] if simulation_success else 'nan',
+        'operation': restrictions_metadata['operation'],
+        'failed_rotors': restrictions_metadata['failed_rotors'],
+        'ang_speed_percentages (%)': restrictions_metadata['ang_speed_percentages'],
+        'mpc_min_phi (rad)': simulation_metadata['mpc_min_phi'] if simulation_success else 'nan',
+        'mpc_max_phi (rad)': simulation_metadata['mpc_max_phi'] if simulation_success else 'nan',
+        'mpc_phi (rad)': simulation_metadata['mpc_mean_phi'] if simulation_success else 'nan',
+        'mpc_std_phi (rad)': simulation_metadata['mpc_std_phi'] if simulation_success else 'nan',
+        'mpc_min_theta (rad)': simulation_metadata['mpc_min_theta'] if simulation_success else 'nan',
+        'mpc_max_theta (rad)': simulation_metadata['mpc_max_theta'] if simulation_success else 'nan',
+        'mpc_mean_theta (rad)': simulation_metadata['mpc_mean_theta'] if simulation_success else 'nan',
+        'mpc_std_theta (rad)': simulation_metadata['mpc_std_theta'] if simulation_success else 'nan',
+        'mpc_min_psi (rad)': simulation_metadata['mpc_min_psi'] if simulation_success else 'nan',
+        'mpc_max_psi (rad)': simulation_metadata['mpc_max_psi'] if simulation_success else 'nan',
+        'mpc_mean_psi (rad)': simulation_metadata['mpc_mean_psi'] if simulation_success else 'nan',
+        'mpc_std_psi (rad)': simulation_metadata['mpc_std_psi'] if simulation_success else 'nan',
+    }
+    simulation_metadata = pd.DataFrame([simulation_metadata])
+    return simulation_metadata
 
 if __name__ == '__main__':
     try:

6dof_quadrotor/simulate_nn.py
@@ -4,10 +4,11 @@ import multirotor
 from neural_network import NeuralNetwork, NeuralNetwork_optuna
 import torch
 from scipy.integrate import odeint
-from plots import plot_states
+from plots import DataAnalyser
 import pandas as pd
 import restriction_handler
 from simulate_mpc import simulate_mpc
+import time
 
 use_optuna_model = True
 
@@ -50,20 +51,9 @@ rst = restriction_handler.Restriction(multirotor_model, T_sample, N, M)
 restriction, output_weights, control_weights, _ = rst.restriction('total_failure', [0,7])
 
 def simulate_neural_network(nn_weights_folder, file_name, t_samples):
-
+    analyser = DataAnalyser()
     nn_weights_path = nn_weights_folder + file_name
 
-    # Import N used in neural network model
-    #N = 0
-    #cmd = 'from ' + nn_weights_folder.replace('/', '.') + 'nn_metadata import N'
-    #print('cmd=',cmd)
-    #exec(cmd)
-    #from dataset_canon.canon_N_50_M_20.global_dataset.nn_metadata import N, M
-
-    print('(Check value) N =', N)
-    if N == 0:
-        print('N was not imported correctly.')
-        return
     # 1. Load Neural Network model
     #device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
     #print(f"Using {device} device")
@@ -83,6 +73,9 @@ def simulate_neural_network(nn_weights_folder, file_name, t_samples):
     normalization_df = pd.read_csv(nn_weights_folder + 'normalization_data.csv', header = None)
 
     # Control loop
+    execution_time = 0
+    waste_time = 0
+    start_time = time.perf_counter()
     for k in range(0, len(t_samples)-1): # TODO: confirmar se é -1 mesmo:
         # Mount input tensor to feed NN
         nn_input = np.array([])
@@ -143,27 +136,72 @@ def simulate_neural_network(nn_weights_folder, file_name, t_samples):
         x_k = odeint(multirotor_model.f2, x_k, t_simulation, args = (f_t_k, t_x_k, t_y_k, t_z_k))
         x_k = x_k[-1]
 
+        waste_start_time = time.perf_counter()
         X_vector.append(x_k)
         u_vector.append(u_k)
         omega_vector.append(np.sqrt(omega_squared))
+        waste_end_time = time.perf_counter()
+        waste_time += waste_end_time - waste_start_time
+    
+    end_time = time.perf_counter()
 
-    return np.array(X_vector), np.array(u_vector), np.array(omega_vector)
+    X_vector = np.array(X_vector)
+    RMSe = analyser.RMSe(X_vector[:, 9:], trajectory[:len(X_vector), :3])
+    execution_time = (end_time - start_time) - waste_time
 
-nn_weights_folder_mse = '../Datasets/Training datasets - v0/'
-weights_file_name_mse = 'model_weights_octorotor.pth'
+    min_phi = np.min(X_vector[:,0])
+    max_phi = np.max(X_vector[:,0])
+    mean_phi = np.mean(X_vector[:,0])
+    std_phi = np.std(X_vector[:,0])
 
-#nn_weights_folder_l1 = 'training_results/25-04-05 - Hover focused dataset N90 M10 - L1Loss/'
-#weights_file_name_l1 = 'model_weights_L1Loss.pth'
+    min_theta = np.min(X_vector[:,1])
+    max_theta = np.max(X_vector[:,1])
+    mean_theta = np.mean(X_vector[:,1])
+    std_theta = np.std(X_vector[:,1])
 
-x_nn, u_nn, omega_nn = simulate_neural_network(nn_weights_folder_mse, weights_file_name_mse, t_samples)
-_, _, simulation_data = simulate_mpc(X0, time_step, T_sample, T_simulation, trajectory, restriction, output_weights, control_weights)
-x_mpc, u_mpc, omega_mpc, _ = simulation_data if simulation_data is not None else [None, None, None, None]
+    min_psi = np.min(X_vector[:,2])
+    max_psi = np.max(X_vector[:,2])
+    mean_psi = np.mean(X_vector[:,2])
+    std_psi = np.std(X_vector[:,2])
 
-#use_optuna_model = False
+    metadata = {
+        'num_iterations': len(t_samples)-1,    
+        'nn_execution_time': execution_time,
+        'nn_RMSe': RMSe,
+        'nn_min_phi': min_phi,
+        'nn_max_phi': max_phi,
+        'nn_mean_phi': mean_phi,
+        'nn_std_phi': std_phi,
+        'nn_min_theta': min_theta,
+        'nn_max_theta': max_theta,
+        'nn_mean_theta': mean_theta,
+        'nn_std_theta': std_theta,
+        'nn_min_psi': min_psi,
+        'nn_max_psi': max_psi,
+        'nn_mean_psi': mean_psi,
+        'nn_std_psi': std_psi,
+    }
 
-#x_mse2, u_vector_mse2, omega_vector_mse2 = simulate_neural_network(nn_weights_folder_mse, weights_file_name_mse, t_samples)
-legend = ['Neural Network', 'MPC', 'Trajectory'] if x_mpc is not None else ['Neural Network', 'Trajectory']
-plot_states(x_nn, t_samples[:np.shape(x_nn)[0]], X_lin=x_mpc, trajectory=trajectory[:len(t_samples)], u_vector=u_nn, omega_vector=omega_nn, legend=legend, equal_scales=True)
-#plot_states(x_l1, t_samples[:np.shape(x_l1)[0]], trajectory=trajectory, u_vector=u_vector_l1, omega_vector=omega_vector_l1)
+    return np.array(X_vector), np.array(u_vector), np.array(omega_vector), metadata
 
-#plot_states(x_mse, t_samples[:np.shape(x_l1)[0]], trajectory=trajectory, u_vector=u_vector_mse, X_lin=x_l1, legend=['MSE', 'MAE'])
+if __name__ == '__main__':
+
+    nn_weights_folder_mse = '../Datasets/Training datasets - v0/'
+    weights_file_name_mse = 'model_weights_octorotor.pth'
+    analyser = DataAnalyser()
+
+    #nn_weights_folder_l1 = 'training_results/25-04-05 - Hover focused dataset N90 M10 - L1Loss/'
+    #weights_file_name_l1 = 'model_weights_L1Loss.pth'
+
+    x_nn, u_nn, omega_nn = simulate_neural_network(nn_weights_folder_mse, weights_file_name_mse, t_samples)
+    _, _, simulation_data = simulate_mpc(X0, time_step, T_sample, T_simulation, trajectory, restriction, output_weights, control_weights)
+    x_mpc, u_mpc, omega_mpc, _ = simulation_data if simulation_data is not None else [None, None, None, None]
+
+    #use_optuna_model = False
+
+    #x_mse2, u_vector_mse2, omega_vector_mse2 = simulate_neural_network(nn_weights_folder_mse, weights_file_name_mse, t_samples)
+    legend = ['Neural Network', 'MPC', 'Trajectory'] if x_mpc is not None else ['Neural Network', 'Trajectory']
+    analyser.plot_states(x_nn, t_samples[:np.shape(x_nn)[0]], X_lin=x_mpc, trajectory=trajectory[:len(t_samples)], u_vector=u_nn, omega_vector=omega_nn, legend=legend, equal_scales=True)
+    #plot_states(x_l1, t_samples[:np.shape(x_l1)[0]], trajectory=trajectory, u_vector=u_vector_l1, omega_vector=omega_vector_l1)
+
+    #plot_states(x_mse, t_samples[:np.shape(x_l1)[0]], trajectory=trajectory, u_vector=u_vector_mse, X_lin=x_l1, legend=['MSE